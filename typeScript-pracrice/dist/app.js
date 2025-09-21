6d704263ff64eccf8193549dad5a8602670c28a5
SubhanAkhtar <superiorcollege740@gmail.com>
2025-09-21 14:09:09 +0500

    Initial commit

---
+++ typeScript-pracrice/dist/app.js
@@ -0,0 +1,12 @@
+"use strict";
+var num = 2;
+console.log(num); // Output: 2
+let str = "Hello World!";
+// Check if the variable is a number
+if (typeof num === 'number') {
+    console.log(num + 1); // Output: 3
+}
+// Check if the variable is a string    
+if (typeof str === 'string') {
+    console.log(str.length); // Output: 13
+}
---
+++ typeScript-pracrice/src/app.ts
@@ -0,0 +1,15 @@
+var num:number = 2;
+console.log(num); // Output: 2
+let str: string = "Hello World!";
+
+// Check if the variable is a number
+
+if (typeof num === 'number') {
+    console.log(num + 1); // Output: 3
+}
+
+// Check if the variable is a string    
+
+if (typeof str === 'string') {
+    console.log(str.length); // Output: 13
+}
